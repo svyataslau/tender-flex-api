e36ad8e85aba48a17ab8cb6fe473c8585b529a33
sbely <svtslv17@gmail.com>
2023-01-19 17:39:14 +0100

    init spring

---
+++ src/test/java/com/tenderflex/tenderflex/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenderflex.tenderflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
