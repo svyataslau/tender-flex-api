fc122f14486be69de07fe3dea71d2118db24e00e
sbely <svtslv17@gmail.com>
2023-01-23 01:11:29 +0100

    FE1-01:added reason why we using gueva

--- src/main/java/com/tenderflex/tenderflex/config/ApplicationUserRole.java
+++ src/main/java/com/tenderflex/tenderflex/config/ApplicationUserRole.java
@@ -7,6 +7,18 @@ import java.util.Set;
 import static com.tenderflex.tenderflex.config.ApplicationUserPermission.*;
 
 public enum ApplicationUserRole {
+    //that's the reason why we could use gueva ---> too much code
+//    STUDENT(new HashSet<>()),
+//    ADMIN(new HashSet<>(){{
+//        add(COURSE_READ);
+//        add(COURSE_WRITE);
+//        add(STUDENT_READ);
+//        add(STUDENT_WRITE);
+//    }}),
+//    ADMINTRAINEE(new HashSet<>(){{
+//        add(COURSE_READ);
+//        add(STUDENT_READ);
+//    }});
     STUDENT(Sets.newHashSet()),
     ADMIN(Sets.newHashSet(COURSE_READ, COURSE_WRITE, STUDENT_READ, STUDENT_WRITE)),
     ADMINTRAINEE(Sets.newHashSet(COURSE_READ, STUDENT_READ));
