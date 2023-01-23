96d6e499dc00c6fdd9cad2200e6d759ae53d20fb
sbely <svtslv17@gmail.com>
2023-01-24 00:52:26 +0100

    FE1-01: deleted useless code

--- src/main/java/com/tenderflex/tenderflex/controllers/UserController.java
+++ src/main/java/com/tenderflex/tenderflex/controllers/UserController.java
@@ -1,11 +1,12 @@
 package com.tenderflex.tenderflex.controllers;
 
-import com.tenderflex.tenderflex.models.ApplicationUser;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @AllArgsConstructor
@@ -19,25 +20,5 @@ public class UserController {
     public ResponseEntity<String> getAllUsers() {
         System.out.println("getAllUsers");
         return ResponseEntity.ok("getUsers");
-    }
-
-    @PostMapping
-    public void registerNewUser(@RequestBody ApplicationUser user) {
-        System.out.println("registerNewUser");
-        System.out.println(user);
-    }
-
-    @DeleteMapping(path = "{userId}")
-    @PreAuthorize("hasAuthority('user:write')")
-    public void deleteUser(@PathVariable("userId") Integer userId) {
-        System.out.println("deleteUser");
-        System.out.println(userId);
-    }
-
-    @PutMapping(path = "{userId}")
-    @PreAuthorize("hasAuthority('user:write')")
-    public void updateUser(@PathVariable("userId") Integer userId, @RequestBody ApplicationUser user) {
-        System.out.println("updateUser");
-        System.out.println(String.format("%s %s", userId, user));
     }
 }
